d385b5d213e227ec770b183879ad28b3861a5e0e
markus1189 <markus1189@gmail.com>
2011-08-28 13:48:04 +0200

    Added spec for Cell class

---
+++ spec/cell_spec.rb
@@ -0,0 +1,20 @@
+# Author: markus1189@gmail.com
+require 'spec_helper'
+
+describe "Cell" do
+  context "when calling dynamically defined methods" do
+
+    before(:each) do
+      @cell = Cell.new
+    end
+
+    it "should respond correctly to all four directions's reader/writer" do
+      %w{north south west east}.each_with_index do |dir, i|
+        @cell.send(dir + '?').should == false
+        @cell.send(dir + '!')
+        @cell.send(dir + '?').should == true
+      end
+    end
+  end
+end
+
