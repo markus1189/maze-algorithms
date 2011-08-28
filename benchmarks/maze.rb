ed386da2161d9d91a4c1e7e62de8711261684fbc
markus1189 <markus1189@gmail.com>
2011-08-28 14:53:51 +0200

    Added benchmark for carve_wall

---
+++ benchmarks/maze.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/maze_algorithms'
+require 'benchmark'
+
+include Benchmark
+
+bmbm(6) do |x|
+  @maze = Maze.new(100,100)
+  x.report("carve_wall") do
+    10_000.times do
+      x = rand(98)
+      y = rand(98)
+      @maze.carve_wall([x,y],[x+1,y])
+    end
+  end
+end
