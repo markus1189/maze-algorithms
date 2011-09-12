1cde4210ca396836da1202300f5b498dd2acdb51
markus1189 <markus1189@gmail.com>
2011-09-12 15:03:30 +0200

    Deleted previous ellers algorithm

--- lib/maze_algorithms/generation/ellers_algorithm.rb
+++ lib/maze_algorithms/generation/ellers_algorithm.rb
@@ -1,133 +1,24 @@
-require 'set'
-require 'pp'
-
-# TODO Union Find!
-
-class Array
-  def count_block_size(start, obj)
-    block_size = 0
-
-    return block_size if self[start] != obj
-
-    offset = start
-    while offset >= 0 && self[offset] == obj
-      block_size += 1
-      offset -= 1
-    end
-
-    offset = start+1
-    while offset < self.size && self[offset] == obj
-      block_size += 1
-      offset += 1
-    end
-
-    block_size
-  end
-end
-
 module MazeAlgorithms
   class EllersAlgorithm
     attr_reader :set
 
     def initialize(maze)
+      @row = (1..maze.width).to_a
+      @union_find = UnionFind.new(@row)
       @maze = maze
-      @set = Array.new(maze.width)
-      assign_missing(@set)
     end
 
-    def generate
-      (@maze.height-1).times do |y|
-        random_join(@maze.get_row(y), y)
-        vertical_connections(y)
-        assign_missing(@set)
-      end
-      #Last row
-      join_adjacent_disjoint(@maze.get_row(@maze.height-1))
-    end
+    def random_join
+      @row.each_cons(2) do |cell_1, cell_2|
+        next unless coin(true,false,false)
 
-    def assign_missing(set)
-      last_index = set.compact.max || -1
+        if not @union_find.same_set?(cell_1,cell_2) && coin
 
-      set.map! do |entry|
-        unless entry then last_index +=1
-        else entry
-        end
-      end
-
-      @set = set
-    end
-
-    def random_join(row, y)
-      (0...row.size).each do |x|
-        new_x = x + coin(1, -1) # randomly choose left of right
-        if valid_different_sets_and_coin(new_x, x)
-          @maze.carve_wall([x,y], [new_x,y])
-
-          ##################### Make sure all set indexes are changed
-          x1 = x
-          x2 = new_x
-          direction = x1 < x2 ? 1 : -1
-          until x2 < 0 || x2 > row.size-1 ||  @maze.has_wall_between?([x1,y],[x2,y])
-            @set[x2] = @set[x1]
-            x1 += direction
-            x2 += direction
-          end
-          #####################
         end
       end
     end
 
-    def join_adjacent_disjoint(row)
-      y = @maze.height-1
-      (0...row.size-1).each do |x| # yes, exclude the two last
-        if @set[x] != @set[x+1]
-          @maze.carve_wall([x,y],[x+1,y])
-        end
-      end
-    end
-
-    # Throws a 'true/false'-coin and if true
-    # Checks:
-    #   - if new_x is valid index for maze
-    #   - if both coords have a different set number
-    #
-    # Used by random_join
-    def valid_different_sets_and_coin(new_x, x)
-      coin(true, false) &&
-        (0...@maze.width) === new_x &&
-        @set[new_x] != @set[x]
-    end
-
-    def vertical_connections(y)
-      old_set = @set
-      new_set = Array.new(old_set.size)
-      offset = 0
-
-      while offset < old_set.size
-        cells_in_set = old_set.count_block_size(offset, old_set[offset])
-        carved_walls = 0
-
-
-        cells_in_set.times do |x|
-          puts "trouble ahead" if offset+x >= old_set.size
-          if cells_in_set == 1 || coin(true, false)
-            @maze.carve_wall([offset+x,y],[offset+x,y+1])
-            new_set[offset+x] = old_set[offset+x]
-            carved_walls += 1
-          end
-          if x == cells_in_set-1 && carved_walls == 0
-            @maze.carve_wall([offset+x,y],[offset+x,y+1])
-            new_set[offset+x] = old_set[offset+x]
-          end
-        end
-
-        offset += cells_in_set
-      end
-
-      @set = new_set
-    end
-
-    def coin(*args)
+    def coin(*args=[true,false])
       args.sample
     end
   end
