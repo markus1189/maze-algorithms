3ddc0d4c69c38b3d2df5d6a3be02c718541724f7
markus1189 <markus1189@gmail.com>
2011-09-13 16:56:07 +0200

    Updated benchmarks

--- benchmarks/generation_bm.rb
+++ benchmarks/generation_bm.rb
@@ -2,49 +2,48 @@ require_relative '../lib/maze_algorithms'
 require 'benchmark'
 
 include Benchmark
+include MazeAlgorithms
 
 bmbm(6) do |x|
   x.report("recursive_10x10") do
     @maze = Maze.new(10,10)
     1_000.times do
-      MazeGeneration::RecursiveBacktracker.generate(@maze)
+      MazeAlgorithms::RecursiveBacktracker.generate(@maze)
     end
   end
 
   x.report("dfs_10x10") do
     @maze = Maze.new(10,10)
     1_000.times do
-      MazeGeneration::DepthFirstSearch.generate(@maze)
+      MazeAlgorithms::DepthFirstSearch.generate(@maze)
     end
   end
 
   x.report("ellers_10x10") do
     @maze = Maze.new(10,10)
     1_000.times do
-      @algo = MazeGeneration::EllersAlgorithm.new(@maze)
-      @algo.generate
+      MazeAlgorithms::EllersAlgorithm.generate(@maze)
     end
   end
 
   x.report("recursive_20x20") do
     @maze = Maze.new(20,20)
     1_000.times do
-      MazeGeneration::RecursiveBacktracker.generate(@maze)
+      MazeAlgorithms::RecursiveBacktracker.generate(@maze)
     end
   end
 
   x.report("dfs_20x20") do
     @maze = Maze.new(20,20)
     1_000.times do
-      MazeGeneration::DepthFirstSearch.generate(@maze)
+      MazeAlgorithms::DepthFirstSearch.generate(@maze)
     end
   end
 
   x.report("ellers_20x20") do
     @maze = Maze.new(20,20)
     1_000.times do
-      @algo = MazeGeneration::EllersAlgorithm.new(@maze)
-      @algo.generate
+      MazeAlgorithms::EllersAlgorithm.generate(@maze)
     end
   end
 end
