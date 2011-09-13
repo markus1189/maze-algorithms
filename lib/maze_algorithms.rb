a8f22e6b3284d599e937b3aada8bd9c2a6a66d9e
markus1189 <markus1189@gmail.com>
2011-09-13 12:22:31 +0200

    DRY version of maze_algorithms.rb

--- lib/maze_algorithms.rb
+++ lib/maze_algorithms.rb
@@ -3,13 +3,21 @@
 
 require 'ap'
 
-require File.dirname(__FILE__) + "/maze_algorithms/datastructure/cell"
-require File.dirname(__FILE__) + "/maze_algorithms/datastructure/maze"
-require File.dirname(__FILE__) + "/maze_algorithms/datastructure/union_find"
+required_files = <<EOS
+maze_algorithms/datastructure/cell
+maze_algorithms/datastructure/maze
+maze_algorithms/datastructure/union_find
 
-require File.dirname(__FILE__) + "/maze_algorithms/generation/depth_first_search"
-require File.dirname(__FILE__) + "/maze_algorithms/generation/recursive_backtracker"
-require File.dirname(__FILE__) + "/maze_algorithms/generation/ellers_algorithm"
+maze_algorithms/generation/depth_first_search
+maze_algorithms/generation/recursive_backtracker
+maze_algorithms/generation/ellers_algorithm
 
-require File.dirname(__FILE__) + "/maze_algorithms/pathfinding/maze_solver"
+maze_algorithms/pathfinding/maze_solver
+EOS
+
+required_files.each_line do |line|
+  line.chomp!
+  next if line.empty?
+  require_relative line
+end
 
