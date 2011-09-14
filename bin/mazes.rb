981ea9d308ac970d0aa04775c60a1948a29fe20c
markus1189 <markus1189@gmail.com>
2011-09-14 15:43:06 +0200

    Improved seed handling

--- bin/mazes.rb
+++ bin/mazes.rb
@@ -6,11 +6,24 @@ include MazeAlgorithms
 
 width  = (ARGV[0] || 20).to_i
 height = (ARGV[1] || 80).to_i
-solve  = (ARGV[2] || true)
-seed   = (ARGV[3] || rand(0xFFFF_FFFF)).to_i
+solve  = (ARGV[2] == "false") ? false : true
+seed   = (ARGV[3] || rand(0xFFFF_FFFF))
 
-srand(seed)
+if seed.class == String
+  conv_seed = seed.each_char.inject(0) { |mem, var| mem + var.ord }
+end
 
-puts MazeSolver.solve(EllersAlgorithm.generate(width, height))
+srand(conv_seed)
+
+maze = EllersAlgorithm.generate(width, height)
+
+maze = EllersAlgorithm.generate(width, height)
+3.times do
+  maze << EllersAlgorithm.generate(width, height)
+end
+
+maze =  MazeSolver.solve(maze) if solve
+
+puts maze
 
 puts "Seed: #{seed}"
